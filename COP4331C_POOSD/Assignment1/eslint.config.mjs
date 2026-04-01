0c7184365193944c2954d608a96ccbf654b0bd60
MrMahon714 <an336953@ucf.edu>
2026-04-01 15:58:11 -0400

    Added the configuration files for the linting tools

---
+++ COP4331C_POOSD/Assignment1/eslint.config.mjs
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+    js.configs.recommended, // This pulls in the "standard" rules from ESLint
+    {
+        languageOptions: {
+            ecmaVersion: "latest",
+            sourceType: "module",
+            globals: {
+                ...globals.browser, // This fixes 'document', 'window', 'XMLHttpRequest', etc.
+                md5: "readonly",    // This fixes the 'md5 is not defined' error
+                module: "readonly"
+            }
+        },
+
+        rules: {
+            "no-unused-vars": "warn",    // Warns you if you create a variable but never use it
+           "no-undef": "error",          // Errors if you try to use a variable that isn't defined
+            "prefer-const": "error",      // Forces you to use 'const' for variables that don't change
+          "eqeqeq": "warn"             //Warns you if you use === instead of == (prevents weird JS bugs)
+        }
+    }
+];
